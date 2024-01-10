f714b0e610faccfd74ea75fe3da93c699ece65bd
Lev Kamenev <kamenev@pop-os.localdomain>
2024-01-10 06:50:18 -0300

    removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,3 @@
-/*#include <iostream>
-#include "../include/matrix.h"
-#include "../include/matrixAVX.h"
-#include "../include/matrixMP.h"
-//#include "../include/matrixMPI.h"
-#include <vector>
-#include <chrono>
-
-std::vector<std::vector<float>> create_matrix(int rows, int cols){
-  std::vector<std::vector<float>> data(rows, std::vector<float>(cols, 0.0f));
-
-  for (size_t i = 0; i < rows; ++i) {
-      for (size_t j = 0; j < cols; ++j) {
-          data[i][j] = static_cast<float>(rand()) / RAND_MAX;
-        }
-    }
-  return data;
-
-}
-*/
 #include <iostream>
 #include "../include/matrix.h"
 #include "../include/matrixAVX.h"
@@ -95,40 +75,3 @@ int main(void) {
 
     return 0;
 }
-/*
-int main(void){
-  */
-  /*std::cout << "Insira o número de linhas e colunas: ";
-  size_t lin, col;
-  std::cin >> lin >> col;
-
-  std::vector<std::vector<float>> dados(lin, std::vector<float>(col, 0.0));
-
-  std::cout << "Insira a matriz linha a linha:\n";
-    for (size_t i = 0; i < lin; ++i) {
-      for (size_t j = 0; j < col; ++j) {
-        std::cout << "Matriz[" << i << "][" << j << "]: ";
-        std::cin >> dados[i][j];
-      }*/
-//  }
-/*
-  const size_t rows = 100;
-  const size_t cols = 100;
-  std::vector<std::vector<float>> mat1 = create_matrix(rows, cols);
-  std::vector<std::vector<float>> mat2 = create_matrix(rows, cols);
-  MatrizAVX matriz1(mat1);
-  MatrizAVX matriz2(mat2);
-  MatrizMP matriz3(mat1);
-  MatrizMP matriz4(mat2);
-  Matriz matriz5(mat1);
-  Matriz matriz6(mat1);
-  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-  MatrizMP result = matriz3 / matriz4;
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>
-    (end - begin).count()/100 << "[µs]" << std::endl;
-
-  
-   
-  return 0;
-}*/
--- src/matrixAVX.cpp
+++ src/matrixAVX.cpp
@@ -91,22 +91,22 @@ MatrizAVX MatrizAVX::operator/(MatrizAVX& obj) {
     if (elementos.empty() || obj.elementos.empty() || elementos[0].size() != obj.elementos.size()) {
         throw std::invalid_argument("Invalid matrix dimensions");
     }
-    __m256 vec_multi_res = _mm256_setzero_ps(); //Initialize vector to zero
-    __m256 vec_mat1 = _mm256_setzero_ps(); //Initialize vector to zero
-    __m256 vec_mat2 = _mm256_setzero_ps(); //Initialize vector to zero
+    __m256 resRegister = _mm256_setzero_ps(); 
+    __m256 aRegister = _mm256_setzero_ps(); 
+    __m256 bRegister = _mm256_setzero_ps(); 
     std::vector<std::vector<float>> res(elementos.size(), std::vector<float>(obj.elementos[0].size(), 0.0));
     for (size_t i = 0; i < elementos.size(); i++){
       for (size_t j = 0; j < obj.elementos[0].size(); j++){
-        vec_mat1 = _mm256_set1_ps(elementos[i][j]);
+        aRegister = _mm256_set1_ps(elementos[i][j]);
         const size_t vectorSamples = (elementos[0].size() / 8) * 8;
         int k = 0;
         for (; k < vectorSamples; k += 8)
         {
-            vec_mat2 = _mm256_loadu_ps(&obj.elementos[j][k]); 
-            vec_multi_res = _mm256_loadu_ps(&res[i][k]);
-            vec_multi_res = _mm256_add_ps(vec_multi_res ,_mm256_mul_ps(vec_mat1, vec_mat2));
+            bRegister = _mm256_loadu_ps(&obj.elementos[j][k]); 
+            resRegister = _mm256_loadu_ps(&res[i][k]);
+            resRegister = _mm256_add_ps(resRegister, _mm256_mul_ps(aRegister, bRegister));
 
-            _mm256_storeu_ps(&res[i][k], vec_multi_res); 
+            _mm256_storeu_ps(&res[i][k], resRegister); 
         }
         for (; k < elementos[0].size(); k++){
           res[i][j] += elementos[i][k] * obj.elementos[k][j];
@@ -115,66 +115,9 @@ MatrizAVX MatrizAVX::operator/(MatrizAVX& obj) {
 
       }
     }
-    /*for (size_t i = 0; i < elementos.size(); ++i) {
-        for (size_t j = 0; j < obj.elementos[0].size(); ++j) {
-            __m256 resRegister = _mm256_setzero_ps();
-            const size_t vectorSamples = (elementos[0].size() / 8) * 8;
-            size_t k = 0;
-
-            for (; k < vectorSamples; k += 8) {
-                if (k < elementos[i].size() && k < obj.elementos.size()) {
-                    __m256 aRegister = _mm256_loadu_ps(&elementos[i][k]);
-                    __m256 bRegister = _mm256_loadu_ps(&obj.elementos[k][j]);
-
-                    resRegister = _mm256_add_ps(resRegister, _mm256_mul_ps(aRegister, bRegister));
-                }
-            }
-
-            if (i < res.size() && j < res[0].size()) {
-                _mm256_storeu_ps(&res[i][j], resRegister);
-            }
-
-            for (; k < elementos[0].size(); ++k) {
-                if (i < elementos.size() && k < elementos[i].size() && k < obj.elementos.size()) {
-                    res[i][j] += elementos[i][k] * obj.elementos[k][j];
-                }
-            }
-        }
-    }*/
-
-    return MatrizAVX(res);
+        return MatrizAVX(res);
 }
 
-/*MatrizAVX MatrizAVX::operator/(const MatrizAVX& obj) {
-    if (elementos[0].size() != obj.elementos.size()) {
-        throw std::invalid_argument("1");
-    }
-
-    std::vector<std::vector<float>> res(elementos.size(), std::vector<float>(obj.elementos[0].size(), 0.0));
-
-    for (size_t i = 0; i < elementos.size(); ++i) {
-        for (size_t j = 0; j < obj.elementos[0].size(); ++j) {
-            __m256 resRegister = _mm256_setzero_ps();
-            const size_t vectorSamples = (elementos[0].size() / 8) * 8;
-            size_t k = 0;
-            for (; k < vectorSamples; k += 8) {
-                __m256 aRegister = _mm256_loadu_ps(&elementos[i][k]);
-                __m256 bRegister = _mm256_loadu_ps(&obj.elementos[k][j]);
-
-                resRegister = _mm256_add_ps(resRegister, _mm256_mul_ps(aRegister, bRegister));
-              }
-            _mm256_storeu_ps(&res[i][j], resRegister);
-
-            for (; k < elementos[0].size(); k++){
-                res[i][j] += elementos[i][k] * obj.elementos[k][j];
-
-            }
-        }
-    }
-
-    return MatrizAVX(res);
-}
-*/
 MatrizAVX MatrizAVX::transpor() {
     std::vector<std::vector<float>> transposta(elementos[0].size(), std::vector<float>(elementos.size(), 0.0));
 
